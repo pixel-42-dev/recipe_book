0dc05422e607fc03544f4b58de17afc1bcc2127e
Vadim Tkachev <vadim.tkachev.42@gmail.com>
2024-12-14 13:56:01 +0300

    Поле ввода select2 для добавления ингредиентов в номом рецепте

--- resources/views/recipe/create.blade.php
+++ resources/views/recipe/create.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Добавить рецепт</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 </head>
 <body>
     <a href="{{ route('index') }}" style="text-decoration: none; color: white; background-color: green; padding: 10px 20px; border-radius: 5px;">Назад</a>
@@ -14,19 +17,21 @@
     <form action="{{ route('recipes.create') }}" method="POST">
         @csrf
         <label for="name">Название рецепта:</label>
-        <input type="text" id="name" name="name" value="{{ old('name') }}" required>
+        <input type="text" id="name" name="name" value="{{ old('name') }}" style="width: 400px" required>
         @error('name')
             <p style="color: red;">{{ $message }}</p>
         @enderror
+        <br>
 
         <label for="description">Описание рецепта:</label>
-        <textarea id="description" name="description" required>{{ old('description') }}</textarea>
+        <textarea id="description" name="description" style="width: 400px" required>{{ old('description') }}</textarea>
         @error('description')
             <p style="color: red;">{{ $message }}</p>
         @enderror
+        <br>
 
         <label for="ingredients">Ингредиенты:</label>
-        <select id="ingredients" name="ingredients[]" multiple required>
+        <select id="ingredients" name="ingredients[]" multiple="multiple" required class="select2" style="width: 400px">
             @foreach($ingredients as $ingredient)
                 <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
             @endforeach
@@ -34,8 +39,18 @@
         @error('ingredients')
             <p style="color: red;">{{ $message }}</p>
         @enderror
+        <br>
 
         <button type="submit">Добавить</button>
     </form>
+
+    <script>
+        $(document).ready(function() {
+            $('.select2').select2({
+                placeholder: 'Выберите ингредиенты',
+                allowClear: true
+            });
+        });
+    </script>
 </body>
 </html>
