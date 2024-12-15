971fc75233732808d377a38ebafc4f55f782df3d
Vladimir Iliev <iliev.vladimir01@yandex.ru>
2024-12-15 21:18:43 +0300

    Добавлена страница 'admin'

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\IngredientController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/search', [MainController::class, 'search'])->name('search');
+
 
 Route::get('/ingredients', [IngredientController::class, 'view'])->name('ingredients');
 Route::post('/ingredients/create', [IngredientController::class, 'create'])->name('ingredients.create');
 
 Route::get('/recipes', [RecipeController::class, 'view'])->name('recipes');
 Route::post('/recipes/create', [RecipeController::class, 'create'])->name('recipes.create');
+
+Route::get('/admin', [AdminController::class, 'view'])->name('admin');
