a1e09751e8aa6875ebc53bd36ce9cbcda07db10d
Nikita Kolankov <kolankovnikita@gmail.com>
2024-12-23 20:10:16 +0300

    main page local

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,25 +10,65 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <h1>Поиск рецептов по ингредиентам</h1>
-    <form action="{{ route('search') }}" method="GET">
-        <label for="ingredients">Выберите ингредиенты:</label>
-        <select id="ingredients" name="ingredients[]" multiple="multiple" required class="select2" style="width: 100%">
-            @foreach($ingredients as $ingredient)
-                <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
-            @endforeach
-        </select>
-        <button type="submit">Искать</button>
-    </form>
-    <a href="{{ route('recipes') }}" style="display: inline-block; margin-top: 10px; text-decoration: none; color: white; background-color: green; padding: 10px 20px; border-radius: 5px;">Добавить рецепт</a>
+    <div class="page">
+    <header class="header">
+        <img class="logo" src="{{ asset('icons/logo.svg') }}" alt="logo">
+        <h1 class="title header__title">Поиск рецептов по ингредиентам</h1>
+    </header>
+    <main class="main">
+        <section class="ingredients-section">
+            <label class="main__label" for="ingredients">Выберите ингредиенты:</label>
+            <form class="form" action="{{ route('search') }}" method="GET">
+                <select id="ingredients" name="ingredients[]" multiple="multiple" required class="select2" style="width: 100%">
+                    @foreach($ingredients as $ingredient)
+                        <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                    @endforeach
+                </select>
+                <button class="search" type="submit">Искать</button>
+            </form>
+            <a class="add-recipe" href="{{ route('recipes') }}">Добавить рецепт</a>
+        </section>
+        <section class="instructions-section">
+            <h2 class="title section__title">Инструкция по использованию:</h2>
+                <ol class="items">
+                    <li class="item">Для того, чтобы найти рецепт, вам необходимо выбрать ингредиенты из списка предложенных.</li>
+                    <li class="item">После выбора ингредиентов нажмите на кнопку "Искать". Вам будет предложен список рецептов, в которых используются ваши ингредиенты.</li>
+                    <li class="item">Если вы не нашли нужный вам рецепт, то помогите найти его остальным! Добавьте его с помощью кнопки "Добавить рецепт".</li>
+                </ol>
+        </section>
+    </main>
 
-        <script>
-            $(document).ready(function() {
-                $('.select2').select2({
-                    placeholder: 'Выберите ингредиенты',
-                    allowClear: true
-                });
+    <footer class="footer">
+        <div class="connection__footer">
+            <h2 class="footer__title">Как с нами связаться:</h2>
+            <div class="connection-item">
+                <img class="email-icon" src="{{ asset('icons/email.svg') }}" alt="email">
+                <p class="footer__text">recipebook@gmail.com</p>
+            </div>
+            <div class="connection-item">
+                <img class="phone-icon" src="{{ asset('icons/phone.svg') }}" alt="phone">
+                <p class="footer__text">+7 (985)-599-55-35</p>
+            </div>
+            <div class="connection-item">
+                <img class="telegram-icon" src="{{ asset('icons/telegram.svg') }}" alt="telegram">
+                <p class="footer__text">@recipebook</p>
+            </div>
+        </div>
+        <img class="qrcode" src="{{ asset('icons/qrcode.svg') }}" alt="qrcode">
+        <div class="about__footer">
+            <h2 class="footer__title">О нас:</h2>
+            <p class="footer__text">Веселый коллектив, любим
+            вкусно покушать</p>
+        </div>
+    </footer>
+    <script>
+        $(document).ready(function() {
+            $('.select2').select2({
+                placeholder: 'Выберите ингредиенты',
+                allowClear: true
             });
-        </script>
-    </body>
+        });
+    </script>
+    </div>
+</body>
 </html>
